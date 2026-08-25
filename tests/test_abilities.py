151ffe47ab657827542b7d940efd3c3996a557e0
kaidence-dempsey <kaidencedempsey@gmail.com>
2026-08-25 12:55:57 -0500

    Finished Ability Tests

--- tests/test_abilities.py
+++ tests/test_abilities.py
@@ -278,13 +278,217 @@ def test_get_all_abilities(db):
         "Test Ability 3"
     }
 
-# def test_get_abilities_by_discipline(db):
+def test_get_abilities_by_discipline(db):
+    """ Test that Ability records can be retrieved from their associated Discipline ID. """
+     # Creating a Discipline to associate with ability1 and ability3
+    discipline1 = disciplines.create_discipline(
+        db=db,
+        name = "Test Discipline",
+        philosophy = None,
+        description = "Test Description"
+    )
 
-# def test_get_abilities_by_nonexistent_discipline(db):
+    # Creating a Discipline to associate with ability2
+    discipline2 = disciplines.create_discipline(
+        db=db,
+        name = "Test Discipline 2",
+        philosophy = None,
+        description = "Test Description"
+    )
 
-# def test_get_abilities_matching_any_tag(db):
+    ability1 = abilities.create_ability(
+        db=db,
+        name = "Test Ability 1",
+        effect = "Test Effect",
+        has_roll = False,
+        partial_effect = None,
+        crit_effect = None,
+        xp_cost = 2,
+        ap_cost = 0,
+        momentum_cost = 0,
+        resource_type = None,
+        resource_cost = None,
+        discipline_id = discipline1.id,
+        tags = []
+    )
 
-# def test_get_abilities_by_nonexistent_tag(db):
+    ability2 = abilities.create_ability(
+        db=db,
+        name = "Test Ability 2",
+        effect = "Test Effect",
+        has_roll = False,
+        partial_effect = None,
+        crit_effect = None,
+        xp_cost = 2,
+        ap_cost = 0,
+        momentum_cost = 0,
+        resource_type = None,
+        resource_cost = None,
+        discipline_id = discipline2.id,
+        tags = []
+    )
 
-# def test_get_abilities_by_no_matching_tag(db):
+    ability3 = abilities.create_ability(
+        db=db,
+        name = "Test Ability 3",
+        effect = "Test Effect",
+        has_roll = False,
+        partial_effect = None,
+        crit_effect = None,
+        xp_cost = 2,
+        ap_cost = 0,
+        momentum_cost = 0,
+        resource_type = None,
+        resource_cost = None,
+        discipline_id = discipline1.id,
+        tags = []
+    )
+
+    retrieved = abilities.get_abilities_by_discipline(db, discipline1.id)
+    assert len(retrieved) == 2
+    assert {a.name for a in retrieved} == {
+        "Test Ability 1",
+        "Test Ability 3"
+    }
+    
+def test_get_abilities_by_nonexistent_discipline(db):
+    """ Test that searching for Ability records given a nonexistent discipline ID returns []. """
+    # Creating a Discipline to associate with the Ability
+    discipline = disciplines.create_discipline(
+        db=db,
+        name = "Test Discipline",
+        philosophy = None,
+        description = "Test Description"
+    )
+
+    ability = abilities.create_ability(
+        db=db,
+        name = "Test Ability",
+        effect = "Test Effect",
+        has_roll = False,
+        partial_effect = None,
+        crit_effect = None,
+        xp_cost = 2,
+        ap_cost = 0,
+        momentum_cost = 0,
+        resource_type = None,
+        resource_cost = None,
+        discipline_id = discipline.id,
+        tags = []
+    )
+
+    result = abilities.get_abilities_by_discipline(db, 100)
+    assert result == []
+
+def test_get_abilities_matching_any_tag(db):
+    """ Test that Ability records can be retrieved via any matching tag. """
+    # Creating a Discipline to associate with the Ability
+    discipline = disciplines.create_discipline(
+        db=db,
+        name = "Test Discipline",
+        philosophy = None,
+        description = "Test Description"
+    )
+
+    tag = tags.create_tag(
+        db=db,
+        name = "Test Tag"
+    )
+
+    # This tag is created to be added to the search, does not link to any ability, but does not result in an empty result.
+    tag2 = tags.create_tag(
+        db=db,
+        name = "Unused Tag"
+    )
+    ability = abilities.create_ability(
+        db=db,
+        name = "Test Ability",
+        effect = "Test Effect",
+        has_roll = False,
+        partial_effect = None,
+        crit_effect = None,
+        xp_cost = 2,
+        ap_cost = 0,
+        momentum_cost = 0,
+        resource_type = None,
+        resource_cost = None,
+        discipline_id = discipline.id,
+        tags = [tag]
+    )
+
+    tag_names = ["Test Tag", "Unused Tag"]
+
+    result = abilities.get_abilities_matching_any_tag(db, tag_names)
+
+    assert result == [ability]
+
+def test_get_abilities_by_nonexistent_tag(db):
+    """ Test that searching for Ability records given a nonexistent tag returns []. """
+    # Creating a Discipline to associate with the Ability
+    discipline = disciplines.create_discipline(
+        db=db,
+        name = "Test Discipline",
+        philosophy = None,
+        description = "Test Description"
+    )
+
+    ability = abilities.create_ability(
+        db=db,
+        name = "Test Ability",
+        effect = "Test Effect",
+        has_roll = False,
+        partial_effect = None,
+        crit_effect = None,
+        xp_cost = 2,
+        ap_cost = 0,
+        momentum_cost = 0,
+        resource_type = None,
+        resource_cost = None,
+        discipline_id = discipline.id,
+        tags = []
+    )
+
+    tag_names = ["Nonexistent"]
+
+    result = abilities.get_abilities_matching_any_tag(db, tag_names)
+
+    assert result == []
+
+def test_get_abilities_by_no_matching_tag(db):
+    """ Test that no Ability records are retrieved given Tags that are not associated to any ability. """
+
+    # Creating a Discipline to associate with the Ability
+    discipline = disciplines.create_discipline(
+        db=db,
+        name = "Test Discipline",
+        philosophy = None,
+        description = "Test Description"
+    )
+
+    tag = tags.create_tag(
+        db=db,
+        name = "Test Tag"
+    )
+
+    ability = abilities.create_ability(
+        db=db,
+        name = "Test Ability",
+        effect = "Test Effect",
+        has_roll = False,
+        partial_effect = None,
+        crit_effect = None,
+        xp_cost = 2,
+        ap_cost = 0,
+        momentum_cost = 0,
+        resource_type = None,
+        resource_cost = None,
+        discipline_id = discipline.id,
+        tags = []
+    )
+
+    tag_names = ["Test Tag"]
+
+    result = abilities.get_abilities_matching_any_tag(db, tag_names)
+
+    assert result == []
 
